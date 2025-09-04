deba0854b20217308f47d24a88d98566a0bad226
Auxence-M <auxencemedja@gmail.com>
2025-09-03 20:17:41 -0400

    Checks if slug exists in database before adding it.

--- functions/function.go
+++ functions/function.go
@@ -15,6 +15,8 @@ import (
 	"cloud.google.com/go/firestore"
 	"cloud.google.com/go/logging"
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 var firestoreClient *firestore.Client
@@ -118,10 +120,29 @@ func handlePostRequest(w http.ResponseWriter, r *http.Request) {
 
 	// Generate slug from url
 	slug := generateToken(longURL.URL)
-	// Write into firestore, slugs are documents id
+
+	// Makes sure slug does not exist in firestore
+	// If it does, create a new one
+	// A slugs is a documents id
+	docSnap, err := firestoreClient.Collection("shortener").Doc(slug).Get(context.Background())
+	if err != nil {
+		if status.Code(err) != codes.NotFound {
+			// Error while cheking if slug exixts
+			logger.Printf("Error checking for slug existance: %v", err)
+			http.Error(w, "Failed shortening url", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	for docSnap.Exists() {
+		slug = generateToken(longURL.URL)
+	}
+
+	// Write into firestore if the generated slug does not exist
 	_, err = firestoreClient.Collection("shortener").Doc(slug).Set(context.Background(), map[string]interface{}{
-		"url":    longURL.URL,
-		"clicks": 0,
+		"original_url": longURL.URL,
+		"created_at":   firestore.ServerTimestamp,
+		"clicks":       0,
 	})
 	if err != nil {
 		logger.Printf("Error saving slug to Firestore: %v", err)
@@ -151,19 +172,21 @@ func handleGetRequest(w http.ResponseWriter, r *http.Request) {
 	// Retrieve the corresponding document from Firestore
 	docSnap, err := firestoreClient.Collection("shortener").Doc(token).Get(context.Background())
 	if err != nil {
-		logger.Printf("Error getting document from Firestore: %v", err)
-		http.Error(w, "Failed to get the corresponding url", http.StatusNotFound)
-		return
-	}
-
-	if !docSnap.Exists() {
-		http.Error(w, "URL not found", http.StatusNotFound)
-		return
+		if status.Code(err) == codes.NotFound {
+			// Short URL does not exist
+			logger.Printf("Document not found in firestore: %v", err)
+			http.Error(w, "Short URL not found", http.StatusNotFound)
+			return
+		} else {
+			logger.Printf("Error checking for slug existance: %v", err)
+			http.Error(w, "Failed shortening url", http.StatusInternalServerError)
+			return
+		}
 	}
 
 	// Extract the URL from the document data
 	data := docSnap.Data()
-	url, ok := data["url"].(string)
+	url, ok := data["original_url"].(string)
 	if !ok {
 		http.Error(w, "Corrupted data", http.StatusInternalServerError)
 		return
